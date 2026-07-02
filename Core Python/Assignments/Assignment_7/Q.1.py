b2e8e6138a9e496af756bb2032066c0575456ce6
Tanuja8605 <tanujakadam95@gmail.com>
2026-07-02 19:40:22 +0530

    Add files via upload

---
+++ Core Python/Assignments/Assignment_7/Q.1.py
@@ -0,0 +1,19 @@
+for i in range(1, 6):
+    for j in range(1, 6 - i):
+        print(' ', end=' ')
+    for j in range(1, 2 * i):
+        if j == 1 or j == 2 * i - 1:
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
+
+for i in range(6-1, 0, -1):
+    for j in range(1, 6 - i):
+        print(' ', end=' ')
+    for j in range(1, 2 * i):
+        if j == 1 or j == 2 * i - 1:
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
